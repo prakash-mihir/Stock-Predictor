86bb6996056a80b71a422a62da2c8c559dd25629
Aditya Prakash <55011564+AdityaPrakash-26@users.noreply.github.com>
2021-08-24 13:29:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-stocks = ('RELIANCE.NS', 'IDFCFIRSTB.NS', 'SBIN.NS', 'ITC.NS')
+stocks = ('RELIANCE.NS', 'IDFCFIRSTB.NS', 'SBIN.NS', 'ITC.NS', 'ZOMATO.BO')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
