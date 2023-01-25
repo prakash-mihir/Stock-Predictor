a0077fc9f317429dfac8ff2601e5811fbdb10ac3
mihir9614 <30488170+mihir9614@users.noreply.github.com>
2023-01-25 20:55:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,9 +59,9 @@ st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+# st.write("Forecast components")
+# fig2 = m.plot_components(forecast)
+# st.write(fig2)
 
 # hide stuff for production deployment.
 # Comment the following lines to enable debug 
