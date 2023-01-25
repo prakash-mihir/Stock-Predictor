965a8d59e0f5801c64d0af51be7b9bc64cc5aeb7
mihir9614 <30488170+mihir9614@users.noreply.github.com>
2023-01-25 21:08:28 +0530

    .

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ def plot_raw_data():
 plot_raw_data()
 
 # Predict forecast with Prophet.
-df_train = data[['Dates','Values']]
-df_train = df_train.rename(columns={"Dates": "ds", "Values": "y"})
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 m = Prophet()
 m.fit(df_train)
